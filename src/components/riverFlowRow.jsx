79cf287d8cddad1a909426ead29c3c899aa7e2f6
Scott Gilbert <scottvtgilbert@gmail.com>
2024-05-17 23:49:46 -0400

    Beginning setup of gauges page fetch.

--- src/components/riverFlowRow.jsx
+++ src/components/riverFlowRow.jsx
@@ -4,12 +4,29 @@ import styles from "./riverFlowRow.module.scss"
 export default function RiverFlowRow({ riverInfo }) {
 
     return (
-        <tr>
-            <td>{riverName}</td>
-            <td>{discharge}</td>
-            <td>{min-max}</td>
-            <td>{gauge}</td>
-            <td>{readingtime}</td>
-        </tr>
+        <div>FlowRow</div>
+        // <tr>
+        //     <td className="name"><a href={`https://creekvt.com/${flows.riverURL}`}>{flows.riverName}</a>
+        //         <br /><br />
+        //         <p className="diff">{flows.riverDifficulty}</p></td>
+        //     <td className="level">
+        //         <div className="levelContainer">
+
+        //             <div className="levelInfo">{flows.gauge1.currentLvl }<span className="mobile-show"><br><br>{flows.gauge1upperLimit }} <hr> {{ this.gauges.gauge1.lowerLimit }}</span>
+        //             </div>
+
+        //                 <div className="flowBar" style="background-image:linear-gradient(0deg, {{this.gauges.gauge1.flowColor}} {{this.gauges.gauge1.percentile}}, white 1%">
+        //                     <span className="curLvl"><div className="top arrow {{this.gauges.gauge1.trend}}"></div>{{ this.gauges.gauge1.lvlChng }}/hr<br /><div className="bottom arrow {{this.gauges.gauge1.trend}}"></div></span>
+        //                 </div>
+
+        //             </div>
+        //             </td>
+        //                 <td className="limits mobile-hide">{{ this.gauges.gauge1.upperLimit }} <hr> {{ this.gauges.gauge1.lowerLimit }}</td>
+        //                 <td className="gaugeName mobile-hide">{{ this.gauges.gauge1.name }}</td>
+        //                 <td className="updateTime mobile-hide">{{ this.gauges.gauge1.reportTime }}</td>
+        //                 <td className="gaugeName mobile-show">{{ this.gauges.gauge1.name }}<br><br>{{ this.gauges.gauge1.reportTime }}</td>
+
+        //                 </tr>
+
     )
 }
--- src/pages/gauges.jsx
+++ src/pages/gauges.jsx
@@ -1,10 +1,46 @@
 //Components
-import Loader from "../components/loader"
+import Loader from "../components/loader";
+//Hooks
+import { useEffect, useState } from "react";
+//Libraries
+import { formatDateTime } from "../utils/formatDateTime";
 
-export default function Gauges(){
+export default function Gauges() {
+    const [status, setStatus] = useState('pending'); //pending, success, error
+
+    useEffect(() => {
+        async function fetchGauges() {
+            try {
+                let today = formatDateTime(new Date());
+                let startMonth = today.month;
+                let startDay = today.day - 1;
+                if (startDay === 0) {
+                    if (startMonth === 3) {
+                        startDay = 28;
+                        startMonth = today.month - 1;
+                    }
+                    else {
+                        startDay = 30;
+                        startMonth = today.month - 1;
+                    }
+                }
+                let formattedToday = `&startDT=${today.year}-${startMonth}-${startDay}&endDT=${today.year}-${today.month}-${today.day - 1}`
+                let url = 'https://nwis.waterservices.usgs.gov/nwis/iv/?format=json&sites=01142500,04296280,04296000,01153000,04296500,01138500,04287000,04283500,01133000,01139800,01140817,01170100,04292810,04294500,04294620,04295500,04292500,04292000,04282795,04282780,04282650,04288295,04289000,04288000,04292750,04294000,04293500,04293000,01134500,04282525,04285800,04285500,01151500,01150900,04282000,04282500,04282576,01135500,04294300,01135150,04280000,04288230,04294140,01154000,01135300,04288225,01334000,04288500,01139000,01155500,01144000,01153550,04284751,04286000,04290500,04285000&parameterCd=00060' + formattedToday;
+                let guageResponse = await fetch(url);
+                let gaugeData = await guageResponse.json();
+                console.log(formattedToday, gaugeData)
+
+            } catch (error) {
+                console.log(`There was an error fetching gauge data: ${error}`)
+                setStatus('error')
+            }
+        }
+        fetchGauges();
+    }, [])
+
     return (
         <div>
-            <Loader bottom_text={"Loading Gauges"}/>
+            <Loader bottom_text={"Loading Gauges"} />
         </div>
     )
 }
