91f7b97fd4240eb2751ff5910b03373553f7d56b
Scott Gilbert <scottvtgilbert@gmail.com>
2024-07-01 22:42:11 -0400

    Setting up map styles

--- src/components/dynamicMap.jsx
+++ src/components/dynamicMap.jsx
@@ -20,20 +20,89 @@ function MyMapComponent({ selectedMapLocation }) {
     useEffect(() => {
         const buildMap = async () => {
             const newMap = new window.google.maps.Map(mapRef.current, {
-                center: { lat: 44, lng: -72.75 },
-                zoom: 7,
+                center: { lat: 44, lng: -72.55 },
+                zoom: 7.75,
+                isFractionalZoomEnabled: true,
                 mapTypeId: "terrain",
                 streetViewControl: false,
                 mapTypeControl: false,
                 styles: [{
                     "featureType": "poi",
+                    // "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
                     "elementType": "labels",
                     "stylers": [
                         {
                             "visibility": "off"
                         }
                     ]
-                }]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "weight": .35,
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.locality",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.province",
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    featureType: 'administrative.province',
+                    elementType: 'geometry.stroke',
+                    stylers: [
+                        { color: '#333333' },
+                        { weight: 2 }
+                    ]
+                },
+                {
+                    featureType: 'administrative.country',
+                    elementType: 'geometry.stroke',
+                    stylers: [
+                        { color: '#333333' },
+                        { weight: 2 }
+                    ]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry.fill',
+                    stylers: [
+                    //   { color: '#0088ff' } // Set your desired water fill color
+                      { color: '#4682b4' } // Set your desired water fill color
+                    ]
+                  }
+                ]
             });
 
             let riverResposne = await fetch("https://creekvt.com/FlowsPageAssets/all_rivers_colorless.geojson")
@@ -41,17 +110,21 @@ function MyMapComponent({ selectedMapLocation }) {
             await newMap.data.addGeoJson(riverJSON)
             newMap.data.forEach(river => river.setProperty("Cool?", "yes"))
             newMap.data.setStyle(function (feature) {
-                if (feature.Fg["Class"] === "IV") {
+                // if (feature.Fg["Class"].match(/^V[\+\-]?$/)) {
+                // if (feature.Fg["Class"].match(/^IV[\+\-]?$/)) {
+                if (feature.Fg["Class"].match(/^III[\+\-]?$/)) {
                     return {
-                        strokeColor: 'blue',
-                        strokeWeight: 3
+                        // strokeColor: '#222299',
+                        strokeColor: '#009922',
+                        strokeWeight: 7,
+                        strokeOpacity: 1
                     }
                 }
                 else {
                     return {
-                        strokeColor: 'black',
-                        strokeWeight: 1,
-                        strokeOpacity: .65
+                        strokeColor: '#996633',
+                        strokeWeight: 3.25,
+                        strokeOpacity: .85
                     }
                 }
             })
