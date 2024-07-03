fea1b890c7b2b32e0138eabc601b6586e41fa7d5
Scott Gilbert <scottvtgilbert@gmail.com>
2024-07-03 00:07:59 -0400

    Moving gauge fetch to inner layout and passing to gauges page using context

--- src/pages/gauges.jsx
+++ src/pages/gauges.jsx
@@ -4,6 +4,7 @@ import RiverFlowRow from "../components/riverFlowRow";
 import GaugesSortBar from "../components/gaugesSortBar";
 //Contexts
 import { RiverContext } from "./innerLayout";
+import { RiverDataWithGaugeInfoContext } from "./innerLayout";
 //Hooks
 import { useContext, useEffect, useState } from "react";
 //Icons
@@ -26,45 +27,13 @@ function GaugeInstructions({setIsModalActive}) {
 }
 
 export default function Gauges() {
-    const [status, setStatus] = useState('pending'); //pending, success, error
-    const [gaugeData, setGaugeData] = useState(null);
     const riverData = useContext(RiverContext).riverData;
-    const [updatedRiverData, setUpdatedRiverData] = useState(null);
+    const updatedRiverData = useContext(RiverDataWithGaugeInfoContext).updatedRiverData;
+    const status = useContext(RiverDataWithGaugeInfoContext).status;
     const [sortedBy, setSortedBy] = useState('curLevel'); //riverName, curLevel, difficulty, location, quality
     const [sortedRiverData, setSortedRiverData] = useState(null);
     const [isModalActive, setIsModalActive] = useState(false)
-
-    console.log(`River data`, riverData)
-    console.log(`Updated river data`, updatedRiverData)
-    console.log(`Gauge data`, gaugeData)
-    console.log(`Sorted river data`, sortedRiverData)
-
-    useEffect(() => {
-        async function fetchGauges() {
-            try {
-                let formattedToday = formatUSGSDateTimeQueryString();
-                let usgsURL = 'https://nwis.waterservices.usgs.gov/nwis/iv/?format=json&sites=01142500,04296280,04296000,01153000,04296500,01138500,04287000,04283500,01133000,01139800,01140817,01170100,04292810,04294500,04294620,04295500,04292500,04292000,04282795,04282780,04282650,04288295,04289000,04288000,04292750,04294000,04293500,04293000,01134500,04282525,04285800,04285500,01151500,01150900,04282000,04282500,04282576,01135500,04294300,01135150,04280000,04288230,04294140,01154000,01135300,04288225,01334000,04288500,01139000,01155500,01144000,01153550,04284751,04286000,04290500,04285000&parameterCd=00060' + formattedToday;
-                let usgsResponse = await fetch(usgsURL);
-                if (usgsResponse.status < 200 || usgsResponse.status > 299) {
-                    throw new Error(`Gauge data fetch error to url: ${usgsURL}`)
-                }
-                let usgsData = await usgsResponse.json();
-                setGaugeData(usgsData.value.timeSeries)
-            } catch (error) {
-                console.log(`There was an error fetching gauge data: ${error}`)
-                setStatus('error')
-            }
-        }
-        fetchGauges();
-    }, [])
-
-    useEffect(() => {
-        if (riverData && gaugeData) {
-            let updatedData = updateRiverGaugeObj(riverData, gaugeData);
-            setUpdatedRiverData(updatedData);
-            setTimeout(() => setStatus('success'), 2000);
-        }
-    }, [riverData, gaugeData])
+    
 
     useEffect(() => {
         if (updatedRiverData) {
@@ -79,9 +48,6 @@ export default function Gauges() {
         }
 
     }, [updatedRiverData, sortedBy])
-
-
-
 
 
 
